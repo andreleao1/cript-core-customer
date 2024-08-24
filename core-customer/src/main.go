e47359ca34a59b93a788c933368b0585c0be29eb
andreleao1 <andre.gustavo.ls@hotmail.com>
2024-08-24 01:56:02 -0300

    configurating context path

--- core-customer/src/main.go
+++ core-customer/src/main.go
@@ -5,10 +5,16 @@ import "github.com/gin-gonic/gin"
 func main() {
 
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	contextPath := router.Group("/core-customer")
+	{
+		contextPath.GET("/users", GetUsers)
+	}
+
 	router.Run()
+}
+
+func GetUsers(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello from core-customer",
+	})
 }
